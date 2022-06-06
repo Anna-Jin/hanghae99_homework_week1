2590814212b67548350e548a305be149dcab079a
Anna-Jin <anna.jin.6848@gmail.com>
2022-06-06 14:48:49 +0900

    필요하지 않은 파일 삭제

--- src/main/java/com/homework/homework/test/dao/TestDao.java
+++
@@ -1,9 +0,0 @@
-package com.homework.homework.test.dao;
-
-import com.homework.homework.test.domain.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestDao extends JpaRepository<Test, String> {
-}
